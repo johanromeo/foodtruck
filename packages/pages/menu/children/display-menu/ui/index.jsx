92200ddb332a8982eead9215a15c25891e286c38
johanromeo <johan_romeo123@live.se>
2025-02-19 10:27:34 +0100

    mvp menu

---
+++ packages/pages/menu/children/display-menu/ui/index.jsx
@@ -0,0 +1,23 @@
+import "./index.css";
+
+function DisplayMenu() {
+  return (
+    <section className="display-menu-container">
+      <div className="display-menu-title">
+        <h2>Meny</h2>
+      </div>
+      <div className="display-menu-items">
+        <p>Karlstad.............9SEK</p>
+        <p>Karlstad.............9SEK</p>
+        <p>Karlstad.............9SEK</p>
+        <p>Karlstad.............9SEK</p>
+        <p>Karlstad.............9SEK</p>
+        <p>Karlstad.............9SEK</p>
+        <p>Karlstad.............9SEK</p>
+        <p>Karlstad.............9SEK</p>
+      </div>
+    </section>
+  );
+}
+
+export { DisplayMenu };
--- packages/pages/menu/ui/index.jsx
+++ packages/pages/menu/ui/index.jsx
@@ -1,12 +1,18 @@
+/* filepath: /C:/Users/jofil/Desktop/foodtruck/packages/pages/menu/ui/index.jsx */
 import "./index.css";
+import { DisplayMenu } from "@foodtruck/display-menu";
 
 function Menu() {
   return (
-    <div className="menu-wrapper">
-      {/* Add menu components here */}
-
-      {/* Here I will place the YYGS logo and the shopping cart icon */}
-      <h1>This is Father Menu</h1>
+    <div className="menu">
+      <header>
+        <img src="/src/assets/white-logo.png" alt="logo" />
+        <div className="cart">
+          <img src="/src/assets/cart-icon.svg" alt="cart icon" />
+          <p>6</p>
+        </div>
+      </header>
+      <DisplayMenu />
     </div>
   );
 }
