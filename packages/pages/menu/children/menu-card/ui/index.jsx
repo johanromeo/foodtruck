08a906109bc36409030f6fed05060cf82c4f0da0
johanromeo <johan_romeo123@live.se>
2025-02-19 21:43:57 +0100

    added comma-separated ingredients list

--- packages/pages/menu/children/menu-card/ui/index.jsx
+++ packages/pages/menu/children/menu-card/ui/index.jsx
@@ -1,11 +1,13 @@
 import "./index.css";
 
 function MenuCard({ item }) {
+  let ingredients = item.ingredients?.join(", ");
+
   return (
     <article className="menu-card-container">
       <div>
         <p>{item.name}</p>
-        <p>{item.ingredients}</p>
+        <p>{ingredients}</p>
       </div>
       <div className="menu-price">
         <p>{item.price} SEK</p>
