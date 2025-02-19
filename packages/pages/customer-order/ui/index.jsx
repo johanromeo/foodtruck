42601efc13cfd4cbbf4ebd2a628dd8dc2a136557
johanromeo <johan_romeo123@live.se>
2025-02-19 11:09:19 +0100

    comment block useNav

--- packages/pages/customer-order/ui/index.jsx
+++ packages/pages/customer-order/ui/index.jsx
@@ -1,15 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import "./index.css";
 
 import { DisplayOrder } from "@foodtruck/display-order";
 
 function CustomerOrder() {
+  const navigate = useNavigate();
+
   return (
     <div>
       <DisplayOrder />
 
       {/* Base components under this line? */}
       <p>TOTAL: 100kr</p>
-      <button onClick={() => alert("User navigates to ETA screen")}>
+      <button
+        onClick={
+          () => alert("User navigates to ETA screen")
+          // onClick={() => navigate("/eta-screen")}
+        }
+      >
         Take my money
       </button>
     </div>
