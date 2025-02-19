89112d5817f16a5446a05296e112c27997115d1f
johanromeo <johan_romeo123@live.se>
2025-02-19 09:16:52 +0100

    added menu and base settings

---
+++ packages/pages/menu/ui/index.jsx
@@ -0,0 +1,14 @@
+import "./index.css";
+
+function Menu() {
+  return (
+    <div className="menu-wrapper">
+      {/* Add menu components here */}
+
+      {/* Here I will place the YYGS logo and the shopping cart icon */}
+      <h1>This is Father Menu</h1>
+    </div>
+  );
+}
+
+export { Menu };
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
+import { Menu } from "@foodtruck/menu";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Menu />
+    </div>
+  );
 }
 
 export default App;
