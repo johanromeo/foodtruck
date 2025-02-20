1a2b097b35a20c0ad18c9adaeb7568f6c50c4de5
johanromeo <johan_romeo123@live.se>
2025-02-20 16:34:01 +0100

    cart state is updated with id of specific menu items

--- packages/pages/menu/ui/index.jsx
+++ packages/pages/menu/ui/index.jsx
@@ -4,8 +4,11 @@ import { useNavigate } from "react-router-dom";
 
 import { DisplayMenu } from "@foodtruck/display-menu";
 
+import { useSelector } from "react-redux";
+
 function Menu() {
   const navigate = useNavigate();
+  const cart = useSelector((state) => state.cart);
 
   return (
     <div className="menu">
@@ -13,7 +16,7 @@ function Menu() {
         <img src="/src/assets/white-logo.png" alt="logo" />
         <div className="cart" onClick={() => navigate("/my-order")}>
           <img src="/src/assets/cart-icon.svg" alt="cart icon" />
-          <p>6</p>
+          <p>{cart.length}</p>
         </div>
       </header>
       <DisplayMenu />
