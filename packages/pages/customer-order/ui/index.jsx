d202710b404d2a5bc533519d25f7065ce9b5ebeb
johanromeo <johan_romeo123@live.se>
2025-02-21 10:56:17 +0100

    items array indeed contains correct ids

--- packages/pages/customer-order/ui/index.jsx
+++ packages/pages/customer-order/ui/index.jsx
@@ -4,20 +4,26 @@ import "./index.css";
 import { DisplayOrder } from "@foodtruck/display-order";
 import { Button } from "@foodtruck/button";
 import { DisplaySum } from "@foodtruck/display-sum";
+import { useSelector } from "react-redux";
 
 function CustomerOrder() {
   const navigate = useNavigate();
+  const cart = useSelector((state) => state.cart);
+
+  // Add all the cart items in an "items" array.
+  // If quantity of an item is > 0, multiple id of a same item will be added, like [1, 1, 2, 5, 6, 6, 6]
+  const items = cart.flatMap((item) => Array(item.quantity).fill(item.id));
+  console.log(items);
+
+  // Call API endpoint to place an order and pass items[] as body.
 
   return (
     <div>
       <DisplayOrder />
-
-      {/* Base components under this line? */}
       <DisplaySum />
       <Button
         type={"button button__black"}
         text={"Take My Money!"}
-        // TODO: Maybe fix onclick so it's cleaner?
         onClick={() => {
           navigate("/eta");
         }}
