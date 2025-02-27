a3423388585d8d08db3c291298f248576475702a
johanromeo <johan_romeo123@live.se>
2025-02-27 10:10:04 +0100

    conditional rendering on /eta

--- packages/pages/eta-screen/ui/index.jsx
+++ packages/pages/eta-screen/ui/index.jsx
@@ -16,9 +16,20 @@ function ETAScreen() {
       </div>
       <div className="eta-body">
         <img src="/src/assets/food-box.png" alt="YYGS logo in red" />
-        <h2>DINA WONTONS TILLAGAS!</h2>
-        <h3>ETA {data?.order.eta}</h3>
-        <p>#{data?.order.id}</p>
+        {isLoading ? (
+          <h2>Inväntar orderstatus.</h2>
+        ) : error ? (
+          <h2>
+            Din orderstatus kunde inte hämtas. Vänligen kontakta Flavoy's
+            Foodtruck.
+          </h2>
+        ) : (
+          <div className="eta-body">
+            <h2>DINA WONTONS TILLAGAS!</h2>
+            <h3>ETA {data?.order.eta}</h3>
+            <p>#{data?.order.id}</p>
+          </div>
+        )}
       </div>
       <div className="eta-button">
         <Button
