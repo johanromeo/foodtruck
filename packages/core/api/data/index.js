cce3ae036ee1c04f130235f8d89751a3a254c272
johanromeo <johan_romeo123@live.se>
2025-02-28 10:18:01 +0100

    exports of unknowns...

--- packages/core/api/data/index.js
+++ packages/core/api/data/index.js
@@ -1,9 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { apiKey, tenantId, baseUrl } from "@foodtruck/api/data/secrets";
 
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://fdnzawlcf6.execute-api.eu-north-1.amazonaws.com",
+    baseUrl: `${baseUrl}`,
   }),
   endpoints: (builder) => ({
     // MENU ENDPOINTS
@@ -12,7 +13,7 @@ export const apiSlice = createApi({
         url: "/menu",
         method: "GET",
         headers: {
-          "x-zocom": "yum-qqO7Lv5Hg29t5hqI",
+          "x-zocom": `${apiKey}`,
         },
       }),
     }),
@@ -22,7 +23,7 @@ export const apiSlice = createApi({
         url: `/menu/${id}`,
         method: "GET",
         headers: {
-          "x-zocom": "yum-qqO7Lv5Hg29t5hqI",
+          "x-zocom": `${apiKey}`,
         },
       }),
     }),
@@ -30,32 +31,22 @@ export const apiSlice = createApi({
     // ORDER ENDPOINTS
     placeOrder: builder.mutation({
       query: (items) => ({
-        url: "/cv5e/orders",
+        url: "/${tenantId}/orders",
         method: "POST",
         headers: {
-          "x-zocom": "yum-qqO7Lv5Hg29t5hqI",
+          "x-zocom": `${apiKey}`,
           "Content-Type": "application/json",
         },
         body: { items },
       }),
     }),
 
-    getOrders: builder.query({
-      query: () => ({
-        url: "/cv5e/orders",
-        method: "GET",
-        headers: {
-          "x-zocom": "yum-qqO7Lv5Hg29t5hqI",
-        },
-      }),
-    }),
-
     getOrderById: builder.query({
       query: (id) => ({
-        url: `/cv5e/orders/${id}`,
+        url: `/${tenantId}/orders/${id}`,
         method: "GET",
         headers: {
-          "x-zocom": "yum-qqO7Lv5Hg29t5hqI",
+          "x-zocom": `${apiKey}`,
         },
       }),
     }),
@@ -66,7 +57,7 @@ export const apiSlice = createApi({
         url: `/receipts/${id}`,
         method: "GET",
         headers: {
-          "x-zocom": "yum-qqO7Lv5Hg29t5hqI",
+          "x-zocom": `${apiKey}`,
         },
       }),
     }),
@@ -77,7 +68,6 @@ export const {
   useGetMenuQuery,
   useGetMenuItemQuery,
   usePlaceOrderMutation,
-  useGetOrdersQuery,
   useGetOrderByIdQuery,
   useGetReceiptByIdQuery,
 } = apiSlice;
---
+++ packages/core/api/data/secrets/index.js
@@ -0,0 +1,5 @@
+const baseUrl = import.meta.env.VITE_BASE_URL;
+const apiKey = import.meta.env.VITE_API_KEY;
+const tenantId = import.meta.env.VITE_TENANT_ID;
+
+export { apiKey, tenantId, baseUrl };
